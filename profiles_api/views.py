64d0972d53baa4ea19d54c8229be8de179a6eaca
Antonio Diaz <j.antonio.udiaz@gmail.com>
2022-01-06 12:32:57 -0600

    ViewSet has been added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -42,3 +43,49 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Deleate a object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(ViewSet):
+    """Test API viewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'method': 'retrieve'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'update'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'method': 'partial_update'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'method': 'destroy'})
